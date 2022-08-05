bddfcb1d14cd10fb89f911f7b5ab7a0c2dcaaeea
Jeremy Pritts <jrpritts7@gmail.com>
2022-08-05 17:23:21 -0400

    Add a YamlAssetParser

---
+++ GuidUpdater.Tests/YamlAssetParserTests.cs
@@ -0,0 +1,22 @@
+﻿using YamlDotNet.RepresentationModel;
+
+namespace GuidUpdater.Tests;
+
+internal class YamlAssetParserTests
+{
+	[Test]
+	public void FileWithStrippedAssetsParseWithoutErrors()
+	{
+		string yamlText = @"%YAML 1.1
+%TAG !u! tag:unity3d.com,2011:
+--- !u!21 &2100000
+Material:
+  serializedVersion: 6
+--- !u!22 &2100001 stripped
+Material:
+  serializedVersion: 6";
+		IList<YamlDocument> documents = YamlAssetParser.ParseYamlStreamFromText(yamlText).Documents;
+		Assert.That(documents.Count, Is.EqualTo(2));
+		Assert.That(documents[1].RootNode.Anchor.Value, Is.EqualTo("2100001 stripped"));
+	}
+}
