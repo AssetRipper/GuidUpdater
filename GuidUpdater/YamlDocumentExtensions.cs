ddbbfc1cad4c9925557583f006d4d8178502d244
Jeremy Pritts <jrpritts7@gmail.com>
2022-08-05 19:30:40 -0400

    Add some more extension classes

---
+++ GuidUpdater/YamlDocumentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.RepresentationModel;
+
+namespace GuidUpdater;
+public static class YamlDocumentExtensions
+{
+	public static bool TryParseName(this YamlDocument document, [NotNullWhen(true)] out string? name)
+	{
+		if (document.RootNode is YamlMappingNode mappingNode 
+			&& mappingNode.TryGetValue("m_Name", out YamlNode? value)
+			&& value is YamlScalarNode valueScalar)
+		{
+			name = valueScalar.Value ?? string.Empty;
+			return true;
+		}
+		name = null;
+		return false;
+	}
+}
---
+++ GuidUpdater/YamlMappingNodeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using YamlDotNet.RepresentationModel;
+
+namespace GuidUpdater;
+
+public static class YamlMappingNodeExtensions
+{
+	public static bool TryGetValue(this YamlMappingNode node, string key, [NotNullWhen(true)] out YamlNode? value)
+	{
+		return node.Children.TryGetValue(key, out value);
+	}
+}
