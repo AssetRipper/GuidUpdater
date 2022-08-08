6b3ca224da7d4e3c0057f0377a135edba8740ca9
Jeremy Pritts <jrpritts7@gmail.com>
2022-08-08 00:09:05 -0400

    Add an identifier map class

---
+++ GuidUpdater/IdentifierMap.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuidUpdater;
+internal static class IdentifierMap
+{
+	private static Dictionary<UnityGuid, UnityGuid> GuidMap { get; } = new();
+	private static Dictionary<PPtrData, PPtrData> PPtrMap { get; } = new();
+
+	public static void Map(UnityGuid oldGuid, UnityGuid newGuid)
+	{
+		if (GuidMap.TryGetValue(oldGuid, out UnityGuid value))
+		{
+			if (value != newGuid)
+			{
+				throw new Exception($"Guid {oldGuid} can't be mapped to {newGuid} because it's already mapped to {value}.");
+			}
+		}
+		else
+		{
+			GuidMap.Add(oldGuid, newGuid);
+		}
+	}
+
+	public static void Map(PPtr oldPPtr, PPtr newPPtr)
+	{
+		ThrowIfInvalidForMapping(oldPPtr);
+		ThrowIfInvalidForMapping(newPPtr);
+		Map(oldPPtr.Guid, newPPtr.Guid);
+		if (PPtrMap.TryGetValue(oldPPtr.ToData(), out PPtrData value))
+		{
+			if (value != newPPtr.ToData())
+			{
+				throw new Exception($"Guid {oldPPtr} can't be mapped to {newPPtr} because it's already mapped to {value}.");
+			}
+		}
+		else
+		{
+			PPtrMap.Add(oldPPtr.ToData(), newPPtr.ToData());
+		}
+	}
+
+	public static bool TryGetNewPPtr(PPtr oldPPtr, out PPtr newPPtr)
+	{
+		ThrowIfInvalidForMapping(oldPPtr);
+		if (PPtrMap.TryGetValue(oldPPtr.ToData(), out PPtrData data))
+		{
+			newPPtr = data.ToPPtr(oldPPtr.Type);
+			return true;
+		}
+		else if (GuidMap.TryGetValue(oldPPtr.Guid, out UnityGuid newGuid))
+		{
+			newPPtr = new PPtr(oldPPtr.FileID, newGuid, oldPPtr.Type);
+			return true;
+		}
+		else
+		{
+			newPPtr = oldPPtr;
+			return false;
+		}
+	}
+
+	public static PPtr GetNewPPtr(PPtr oldPPtr)
+	{
+		TryGetNewPPtr(oldPPtr, out PPtr newPPtr);
+		return newPPtr;
+	}
+
+	private static void ThrowIfInvalidForMapping(PPtr pptr)
+	{
+		if (pptr.IsNull || pptr.IsMissing || pptr.IsInternal || pptr.IsIntraFile)
+		{
+			throw new ArgumentException($"Invalid for mapping: {pptr}", nameof(pptr));
+		}
+	}
+
+	private static PPtrData ToData(this PPtr pptr) => new PPtrData(pptr);
+
+	private readonly record struct PPtrData(long FileID, UnityGuid Guid)
+	{
+		public PPtrData(PPtr pptr) : this(pptr.FileID, pptr.Guid) { }
+		public PPtr ToPPtr(AssetType type) => new PPtr(FileID, Guid, type);
+		public override string ToString() => ToPPtr(AssetType.Serialized).ToString();
+	}
+}
--- GuidUpdater/PPtr.cs
+++ GuidUpdater/PPtr.cs
@@ -1,14 +1,29 @@
-﻿namespace GuidUpdater;
+﻿using System;
+
+namespace GuidUpdater;
 
 public readonly record struct PPtr(long FileID, UnityGuid Guid, AssetType Type)
 {
 	public PPtr(long FileID) : this(FileID, UnityGuid.Zero, AssetType.Serialized) { }
+	public PPtr(long FileID, AssetType Type) : this(FileID, UnityGuid.Zero, Type) { }
 
 	public override string ToString()
 	{
 		return IsIntraFile
 			? $"{{fileID: {FileID}}}"
 			: $"{{fileID: {FileID}, guid: {Guid}, type: {(byte)Type}}}";
+	}
+
+	public PPtr ToIntraFile()
+	{
+		return new PPtr(FileID, Type);
+	}
+
+	public PPtr ToInterFile(UnityGuid guid)
+	{
+		return IsIntraFile || Guid == guid 
+			? new PPtr(FileID, Type) 
+			: throw new ArgumentException(null, nameof(guid));
 	}
 
 	/// <summary>
