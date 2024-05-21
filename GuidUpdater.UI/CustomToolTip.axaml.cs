c50e106b83f4892c31413814f17f9e115bbb41cd
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-05-21 01:12:56 -0400

    Add files

---
+++ GuidUpdater.UI/CustomToolTip.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace GuidUpdater.UI;
+
+public partial class CustomToolTip : UserControl
+{
+	public static readonly StyledProperty<string?> MessageProperty = AvaloniaProperty.Register<CustomToolTip, string?>(nameof(Message), defaultValue: "Test");
+
+	public string? Message
+	{
+		get => GetValue(MessageProperty);
+		set => SetValue(MessageProperty, value);
+	}
+
+	public CustomToolTip()
+	{
+		InitializeComponent();
+		((StyledElement?)Content)!.DataContext = this;
+	}
+}
---
+++ GuidUpdater.UI/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GuidUpdater.UI;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
